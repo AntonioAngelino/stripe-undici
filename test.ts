4df1a9a9aefeeb37c22dba13db973115a225a54c
Antonio Angelino <antonio@optimabase.com>
2024-12-18 18:48:23 +0000

    updated demo file

--- test.ts
+++ test.ts
@@ -1,13 +1,13 @@
-import { fetch } from 'undici';
+import Stripe from "stripe";
 
-const skey: string = "test";
+const skey: string = "sk_test_xxxx3i";
 
 const stripe = new Stripe(skey, {
-  apiVersion: '2024-06-20',
-  httpClient: Stripe.createFetchHttpClient(fetch)
+  apiVersion: "2024-06-20",
+  httpClient: Stripe.createFetchHttpClient(fetch),
 });
 const invoicesResponse = await stripe.invoices.list({
-            customer: props.customerId,
-            starting_after: "",
-            limit: 10,
+  customer: "cus_RIhjpxDIcl7LJe",
+  starting_after: "",
+  limit: 10,
 });
